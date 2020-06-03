9c99368fc116f485f7664618263a07d807ff6aa7
zhurasique <berezovsky.serega@gmail.com>
2020-06-03 17:07:39 +0200

    added FRONT-END to a project

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,30 @@
+let departmentApi = "/api/department";
+let typeApi = "/api/type";
+let productApi = "/api/product";
+
+var vue = new Vue({
+    el: "#app",
+    data: function(){
+        return {
+            departments: []
+        }
+    },
+
+    methods: {
+        loadDepartments: function () {
+            axios({
+                method: "get",
+                url: departmentApi
+            })
+                .then(response => {
+                    this.departments = response.data;
+                }).catch(error => {
+                console.log(error);
+            });
+
+        },
+    },
+        created: function () {
+            this.loadDepartments(this.departments);
+        }
+});
