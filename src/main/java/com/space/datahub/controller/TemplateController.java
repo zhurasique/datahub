d1e2871ab39e72d9b29ffd96db0a1fd063c324a1
zhurasique <berezovsky.serega@gmail.com>
2020-06-05 00:21:37 +0200

    added template controller

---
+++ src/main/java/com/space/datahub/controller/TemplateController.java
@@ -0,0 +1,16 @@
+package com.space.datahub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TemplateController {
+
+    @RequestMapping("/")
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+}
