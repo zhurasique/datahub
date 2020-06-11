e3d9a0d8a881335f5ab8fd9eef304b69673e5878
zhurasique <berezovsky.serega@gmail.com>
2020-06-11 18:51:40 +0200

    fixed security bugs

--- src/main/java/com/space/datahub/security/SecurityConfiguration.java
+++ src/main/java/com/space/datahub/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.space.datahub.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
+        web.ignoring().antMatchers(HttpMethod.POST,"/api/user");
     }
 
     @Override
