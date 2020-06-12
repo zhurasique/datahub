53e9ab39f7f206a7461bf33f9f5e173dc39c8120
zhurasique <berezovsky.serega@gmail.com>
2020-06-12 21:37:29 +0200

    some vanilla changes

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,6 +4,7 @@ let categoryApi = "/api/category/";
 
 let chosenDepartmentId;
 let departmentsId = [];
+let memory;
 
 var departments = new Vue({
     el: "#departments",
@@ -83,21 +84,21 @@ var types = new Vue({
 document.addEventListener('DOMContentLoaded', function(){
     setTimeout(function() {
         let departments = document.getElementsByName("department");
-
         let type = document.getElementById("types");
 
         for (let i = 0; i < departments.length; i++) {
             departments[i].addEventListener('mouseover', function()  {
+                memory = departments[i];
                 chosenDepartmentId = departmentsId[i].id;
                 types.loadTypes();
                 types.loadCategories();
                 type.style.display = "flex";
+                for (let j = 0; j < departments.length; j++) {
+                    departments[j].style.color = "black";
+                    departments[j].style.filter = "unset";
+                }
                 departments[i].style.color = "#ff598a";
                 departments[i].style.filter = "invert(51%) sepia(65%) saturate(2480%) hue-rotate(310deg) brightness(101%) contrast(101%)";
-            }, false);
-            departments[i].addEventListener('mouseout',  function() {
-                departments[i].style.color = "black";
-                departments[i].style.filter = "unset";
             }, false);
         }
     }, 100);
