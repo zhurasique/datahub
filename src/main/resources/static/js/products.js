af41280a3c2a612f7868a02b546eb29cef739df5
zhurasique <berezovsky.serega@gmail.com>
2020-06-21 20:09:58 +0200

    added filters by price and sorting

--- src/main/resources/static/js/products.js
+++ src/main/resources/static/js/products.js
@@ -13,12 +13,26 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
+function addCurrencies(){
+    setTimeout(function() {
+        let val = document.getElementsByClassName("price");
+        for(let i = 0; i < val.length; i++){
+            if(isInteger(val[i].textContent)){
+                val[i].innerHTML = val[i].textContent.toString() + ",00 zł";
+            }else{
+                val[i].innerHTML = val[i].textContent.toString().trim().replace(/\./g,",") + " zł";
+            }
+        }
+    }, 200);
+}
+
 function isInteger(num) {
     return (num ^ 0) === num;
 }
 
 var byType = getParameterByName('type');
 var byCategory = getParameterByName('category');
+var bySearch = getParameterByName('search')
 
 var qParam = '';
 var endPoint = '';
@@ -39,7 +53,20 @@ var products = new Vue({
         return {
             products: [],
             images: [],
-            links: []
+            links: [],
+            byType: byType,
+            byCategory: byCategory,
+            bySearch: bySearch,
+            categories: [],
+            sortType: 'sort',
+            sortOptions: [
+                { text: 'Najnowsze', value: 'newest' },
+                { text: 'Cena: od najwyższej', value: 'expensive' },
+                { text: 'Cena: od najniższej', value: 'cheap' }
+            ],
+            filteredProducts: [],
+            filteredImages: [],
+            filteredLinks: []
         }
     },
 
@@ -53,11 +80,6 @@ var products = new Vue({
                 .then(response => {
                     this.products = response.data;
                     for(let i = 0; i < this.products.length; i++) {
-                        if(isInteger(this.products[i]["price"])){
-                            this.products[i]["price"] = this.products[i]["price"].toString() + ",00 zł";
-                        }else{
-                            this.products[i]["price"] = this.products[i]["price"].toString().trim().replace(/\./g,",") + " zł";
-                        }
                         this.links.push("/product?product=" + this.products[i].id);
                     }
                 }).catch(error => {
@@ -75,15 +97,103 @@ var products = new Vue({
 
                     for(let i = 0; i < this.images.length; i++)
                         this.images[i]["image"] = "http://localhost/dashboard/images/datahub/" + this.images[i]["image"];
-
                 }).
             catch( error => {
                 console.log(error);
             });
         },
+
+        loadCategories: function () {
+            axios({
+                method: "get",
+                url: categoryApi + "type/name?type=" + byType
+            })
+                .then( response => {
+                   this.categories = response.data;
+                }).
+            catch( error => {
+                console.log(error);
+            });
+        },
+
+        filterPrice: function (price) {
+            this.loadProducts();
+            this.loadImages();
+
+            this.filteredLinks = [];
+            this.filteredImages = [];
+            this.filteredProducts = [];
+
+            setTimeout(function() {
+
+                for(let i = 0; i < products.products.length; i++){
+                    if (products.products[i].price < price){
+                        console.log("sliced");
+                        products.filteredProducts.push(products.products[i]);
+                        products.filteredImages.push(products.images[i]);
+                        products.filteredLinks.push(products.links[i]);
+                    }
+                }
+
+                products.products = [];
+                products.images = [];
+                products.links = [];
+
+                for(let i = 0; i < products.filteredProducts.length; i++){
+                    products.products.push(products.filteredProducts[i]);
+                    products.images.push(products.filteredImages[i]);
+                    products.links.push(products.filteredLinks[i]);
+                }
+                addCurrencies();
+            }, 200);
+
+        },
+
+        sortBy: function (sortType) {
+            if(sortType === "expensive"){
+                for(let i = 0; i < this.products.length - 1; i++) {
+                    for (let j = 0; j < this.products.length - i - 1; j++) {
+                        if (this.products[j].price < this.products[j + 1].price) {
+                            var tmp = [];
+                            tmp.push(this.products[j]);
+                            this.products[j] = this.products[j + 1];
+                            this.products[j + 1] = tmp[0];
+                        }
+                    }
+                }
+            }else if(sortType === "cheap"){
+                for(let i = 0; i < this.products.length - 1; i++) {
+                    for (let j = 0; j < this.products.length - i - 1; j++) {
+                        if (this.products[j].price > this.products[j + 1].price) {
+                            var tmp = [];
+                            tmp.push(this.products[j]);
+                            this.products[j] = this.products[j + 1];
+                            this.products[j + 1] = tmp[0];
+                        }
+                    }
+                }
+            }else if(sortType === "newest"){
+                this.loadProducts();
+            }
+        }
     },
     created: function () {
         this.loadProducts(this.products);
         this.loadImages(this.images);
+
+        if(byType !== undefined){
+            this.loadCategories(this.categories);
+        }
     }
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    addCurrencies();
+
+    document.getElementById("sort").addEventListener("change", function(e) {
+        console.log(this.value);
+        if (this.value === "newest") {
+            addCurrencies();
+        }
+    });
 });
